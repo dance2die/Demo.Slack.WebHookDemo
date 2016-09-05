5fd0e7656f1125d167765a792ff8ff0a13a24a5f
dance2die <dontbothermewithspam@gmail.com>
2016-09-05 10:55:59 -0400

    set up bower and gulp

---
+++ Demo.Slack.WebHookDemo.WebEntry/gulpfile.js
@@ -0,0 +1,10 @@
+﻿// http://weblogs.asp.net/fredriknormen/setting-up-gulp-and-bower-for-a-asp-net-mvc-project-in-visual-studio-2013
+
+var gulp = require('gulp');
+var bower = require('gulp-bower');
+
+gulp.task('bower', function () {
+	return bower({ layout: "byComponent" });
+});
+
+gulp.task('default', ['bower']);
